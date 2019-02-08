0c0d44bb77c379dd94cba02eb202b05dc472f6d9
Jason Qu <fishinsea2014@gmail.com>
2019-02-08 20:41:15 +1300

    First part, new features of c# 6 and 7

---
+++ ConsoleApp1/ConsoleApp1/People.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class People
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
