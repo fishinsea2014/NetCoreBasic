61da9f6bdb94e783a2b397884287180a94157897
Jason Qu <fishinsea2014@gmail.com>
2019-02-11 21:39:00 +1300

    Step 1

---
+++ ConsoleApp1/JQ.NetCore2.2.MVC6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace JQ.NetCore2._2.MVC6
+{
+    /// <summary>
+    /// .Net core integrate the Kestrel web server
+    /// IIS can only play an role of Reverse Proxy
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>() //Startup class is the core class
+                .Build();
+    }
+}
---
+++ ConsoleApp1/NetCore2.2/Controllers/UerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NetCore2._2.Controllers
+{
+    public class UerController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
